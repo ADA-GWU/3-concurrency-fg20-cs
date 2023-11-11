42814aaa7fa2a79a3baa5161bbd89aa129432f69
fidanguliyeva <f2guliyeva@gmail.com>
2023-11-11 19:43:44 +0400

    optimized parallel mode and did some refactoring

--- concurrency.cpp
+++ concurrency.cpp
@@ -5,7 +5,7 @@
 #include <opencv2/highgui.hpp>
 #include <thread>
 #include <vector>
-// #include <omp.h>
+
 using namespace std;
 using namespace cv;
 
@@ -13,128 +13,118 @@ int limit(int x){
   return (x > 255)? 255 : x;
 }
 
-Vec3b rgbAverage(Mat img, int x, int y, int size){
+Vec3b rgbAverage(Mat img, int x, int y, int size_x, int size_y){
   int r = 0, g = 0, b = 0;
-  for (int i = y; i < (y+size); i++) {
-    for (int j = x; j < (x+size); j++){
+  for (int i = y; i < (y+size_y); i++) {
+    for (int j = x; j < (x+size_x); j++){
       r += img.at<Vec3b>(i, j)[0];
       g += img.at<Vec3b>(i, j)[1];
       b += img.at<Vec3b>(i, j)[2];
     }
   }
   Vec3b color;
-    color[0] = limit(r / (size*size));
-    color[1] = limit(g / (size*size));
-    color[2] = limit(b / (size*size));
+    color[0] = limit(r / (size_x*size_y));
+    color[1] = limit(g / (size_x*size_y));
+    color[2] = limit(b / (size_x*size_y));
 
   return color;
 }
 
-int addition(int q, int div){
-  int rest = div - (q % div);
-  if(rest == div) return 0;
-  return rest;
-}
-
-void copyTo(Mat source, Mat dest){
-  int sRows = source.rows, sCols = source.cols;
-  dest.setTo(Scalar(0,0,0));
-  for (int i = 0; i < sRows; i++){
-    for (int j = 0; j < sCols; j++){
-      dest.at<Vec3b>(i, j) = source.at<Vec3b>(i, j);
+void quantize(Mat img, int col, int row, int size_col, int size_row){
+  Vec3b color;
+  color = rgbAverage(img, col, row, size_col, size_row);
+  for (int i = row; i < row+size_row; i++) {
+    for (int j = col; j < col+size_col; j++){
+      img.at<Vec3b>(i, j) = color;
     }
   }
 }
 
-Mat preprocess(Mat img, int size){
+void quantizeImageParallel(Mat img, int size){
   
-  int newRows = img.rows + addition(img.rows, size);
-  int newCols = img.cols + addition(img.cols, size);
-  Mat newImage(newRows, newCols, CV_8UC3);
-  copyTo(img, newImage);
-
-  return newImage;
-}
-
-// void quantizeImageParallel(Mat newImage, int yPos1, int blockSize,  int size, string windowName){
-  
-//   Vec3b color;
-//   int y;
-//   for (y = yPos1; y < (yPos1 + yPos1*blockSize); y+=size) {
-//     for (int x = 0; x < newImage.cols; x+=size) {
-//       // imshow(windowName, newImage);
-//       color = rgbAverage(newImage, x, y, size);
-//       for (int i = y; i < y+size; i++) {
-//         for (int j = x; j < x+size; j++){
-//           newImage.at<Vec3b>(i, j) = color;
-//         }
-//       }
-//       // int k = waitKey(1);
-//     }
-//   }
-//   cout << "Thread id: " << yPos1 << endl;
-//   cout << "current last pos: " << y << endl;
-//   cout << "total rows: " << newImage.rows << endl;
-//   if ((newImage.rows - y) < blockSize){
-//     for (;y < newImage.cols; y+=size) {
-//       for (int x = 0; x < newImage.cols; x+=size) {
-//         // imshow(windowName, newImage);
-//         color = rgbAverage(newImage, x, y, size);
-//         for (int i = y; i < y+size; i++) {
-//           for (int j = x; j < x+size; j++){
-//             newImage.at<Vec3b>(i, j) = color;
-//           }
-//         }
-//         // int k = waitKey(1);
-//       }
-//     }
-//   }
-  
-//   // imshow(windowName, newImage);
-//   // int key = waitKey(0);
-// }
-
-
-void quantizeImage(Mat img, int size){
-
-  int height = img.rows;
-  int width = img.cols;
-
-  int newRows = height + addition(height, size);
-  int newCols = width + addition(width, size);
-  Mat newImage(newRows, newCols, CV_8UC3);
-
-  cout << newImage.rows << endl;
-  cout << newImage.cols << endl;
-
-  copyTo(img, newImage);
-
-  cout << newImage.rows << endl;
-  cout << newImage.cols << endl;
-
   namedWindow("Image", WINDOW_NORMAL);
-  resizeWindow("Image", 600, 600);
-  Vec3b color;
-  for (int y = 0; y < newRows; y+=size) {
-    for (int x = 0; x < newCols; x+=size) {
-      imshow("Image", newImage);
-      color = rgbAverage(newImage, x, y, size);
-      for (int i = y; i < y+size; i++) {
-        for (int j = x; j < x+size; j++){
-          newImage.at<Vec3b>(i, j) = color;
+  unsigned int nThreads = thread::hardware_concurrency();
+  if(nThreads == 0) nThreads = 4;
+  vector<thread> threads;
+
+  int segmentRow = (img.rows - img.rows%size)/nThreads;
+  int fittedCols = img.cols - img.cols%size;
+  int fittedRows = segmentRow - segmentRow%size;
+
+  for(int k = 0; k < segmentRow; k+=size){
+    for (int x = 0; x < img.cols; x+=size){
+      if(x < (fittedCols) && k < (fittedRows)){
+        int i = 0;
+        for (int y = k; i < nThreads; i++, y = k+i*segmentRow){
+          threads.push_back(thread(quantize, img, x, y, size, size));
+          threads[i].join();
+        }
+        for (int j = 0; j < nThreads; j++){
+          threads.pop_back();
         }
       }
-      int k = waitKey(1);
+        
+      if(x >= (fittedCols) && k >= (fittedRows)){
+        int i = 0;
+        for (int y = k; i < nThreads; i++, y = k + i*segmentRow){
+          threads.push_back(thread(quantize, img, x, y, img.cols%size, segmentRow%size));
+          threads[i].join();
+        }
+          
+        for (int j = 0; j < nThreads; j++){
+          threads.pop_back();
+        }
+      }
+
+      if(x >= (fittedCols) && k < (fittedRows)){
+        int i = 0;
+        for (int y = k; i < nThreads; i++, y = k+i*segmentRow){
+          threads.push_back(thread(quantize, img, x, y, img.cols%size, size));
+          threads[i].join();
+        }
+
+        for (int j = 0; j < nThreads; j++){
+          threads.pop_back();
+        }
+      }
+      
+      if(x < (fittedCols) && k >= (fittedRows)){
+        int i = 0;
+        for (int y = k; i < nThreads; i++, y = k+i*segmentRow){
+          threads.push_back(thread(quantize, img, x, y, size, segmentRow%size));
+          threads[i].join();
+        }
+          
+        for (int j = 0; j < nThreads; j++){
+          threads.pop_back();
+        }
+      }
+      imshow("Image", img);
+      waitKey(100);
+      imwrite("result_parallel.jpg", img);
     }
   }
-  imshow("Image", newImage);
-  int key = waitKey(0);
-  imwrite("result.jpg", newImage);
-}
 
-void displayImage(Mat img){
+  int rest = img.rows  - segmentRow*nThreads;
+  if(rest > 0){
+    for (int x = 0; x < img.cols; x+=size){
+      if(x < (fittedCols)){
+        thread t(quantize, img, x, segmentRow*nThreads, size, rest);
+        t.join();
+      }
+      else{
+        thread t(quantize, img, x, segmentRow*nThreads, img.cols%size, rest);
+        t.join(); break;
+      }
+      imshow("Image", img);
+      waitKey(100);
+      imwrite("result_parallel.jpg", img);
+    }
+  }
   imshow("Image", img);
   waitKey(0);
+  imwrite("result_parallel.jpg", img);
+
 }
 
 void quantizeImageSequential(Mat img, int size){
@@ -149,58 +139,23 @@ void quantizeImageSequential(Mat img, int size){
     for (x = 0; x < width; x+=size) {
       imshow("Image", img);
       if(x < (width-width%size) && y < (height-height%size)){
-        color = rgbAverage(img, x, y, size);
-        for (int i = y; i < y+size; i++) {
-          for (int j = x; j < x+size; j++){
-            img.at<Vec3b>(i, j) = color;
-          }
-        }
-      }
-      if(x >= (width-width%size) && y >= (height-height%size)){
-        for (int i = y; i < height; i++) {
-          for (int j = x; j < width; j++){
-            img.at<Vec3b>(i, j) = color;
-          }
-        }
-      }
-      if(x >= (width-width%size) && y < (height-height%size)){
-        for (int i = y; i < y+size; i++) {
-          for (int j = x; j < width; j++){
-            img.at<Vec3b>(i, j) = color;
-          }
-        }
+        quantize(img, x, y, size, size);
       }
       if(x < (width-width%size) && y >= (height-height%size)){
-        for (int i = y; i < height; i++) {
-          for (int j = x; j < x+size; j++){
-            img.at<Vec3b>(i, j) = color;
-          }
-        }
+        quantize(img, x, y, size, height%size);
+      }
+      if(x >= (width-width%size) && y < (height-height%size)){
+        quantize(img, x, y, width%size, size);
+      }
+      else{
+        quantize(img, x, y, width%size, height%size);
       }
       waitKey(1);
     }
   }
   imshow("Image", img);
   waitKey(0);
-  imwrite("result.jpg", img);
-}
-
-void splitImage(vector<Mat>& segments, Mat img, int blockSize){
-  int x = 0;
-  for (int i = 0; i < segments.size()-1; i++){
-    segments[i] = Mat(img, Rect(0, x, img.cols, blockSize));
-    x += blockSize;
-  } 
-}
-
-void quantizeImageParallel(Mat img, int col, int row, int size){
-  Vec3b color;
-  color = rgbAverage(img, col, row, size);
-  for (int i = row; i < row+size; i++) {
-    for (int j = col; j < col+size; j++){
-      img.at<Vec3b>(i, j) = color;
-    }
-  }
+  imwrite("result_sequential.jpg", img);
 }
 
 int main(int argc, char *argv[]){
@@ -222,84 +177,8 @@ int main(int argc, char *argv[]){
 
   if(mode == "S")
     quantizeImageSequential(img, size);
-  if(mode == "M"){
-    namedWindow("Image", WINDOW_NORMAL);
-
-    unsigned int nThreads = thread::hardware_concurrency();
-    if(nThreads == 0) nThreads = 4;
-
-
-    int segmentRow = (img.rows - img.rows%size)/nThreads;
-
-    vector<thread> threads;
-
-    for(int k = 0; k < segmentRow; k+=size){
-      for (int x = 0; x < img.cols; x+=size){
-        int i = 0;
-        for (int y = k; i < nThreads; i++, y = k+i*segmentRow){
-          threads.push_back(thread(quantizeImageParallel, img, x, y, size));
-        }
-        cout << threads.size()<< endl;
-
-        imshow("Image", img);
-        waitKey(10);
-        imwrite("result.jpg", img);
-
-        for (int j = 0; j < nThreads; j++){
-          threads[j].join(); 
-        }
-        for (int j = 0; j < nThreads; j++){
-          threads.pop_back();
-        }
-      }
-    }
-    imshow("Image", img);
-    waitKey(0);
-    imwrite("result.jpg", img);
-    
-
-    
-
-    // for(int x = 0; x < img.cols; x+=size){
-    //   for (int y = i*segmentRow; y < (i*segmentRow + segmentRow); y+=size, i++){
-    //     thread t(quantizeImageParallel, img, x, y, size);
-    //   }
-    // }
-
-    // imshow("Image", img);
-    // waitKey(0);
-    // imwrite("result.jpg", img);
-
-
-
-
-
-
-
-
-
-    // int blockSize = size * img.rows / (size*nThreads);
-
-    // vector<Mat> img_segments;
-    // img_segments.resize(nThreads+1);
-    // splitImage(img_segments, img, blockSize);
-
-    // vector<thread> threads;
-
-    // for (int x = 0; x < img.cols; x+=size) {
-    //   for (int i = 0; i < nThreads; i++){
-    //       // imshow("Image", img_segments[i]);
-    //       // waitKey(1);
-    //     for(int y = 0; y < img.rows; y+=size)
-    //       threads.push_back(thread(quantizeImageParallel, img, size, x,y));
-
-    //     threads[i].join();
-
-    //   }
-
-    // }
-    // imwrite("result.jpg", img);
-  }
+  if(mode == "M")
+    quantizeImageParallel(img, size);
 
   return 0;
 }
